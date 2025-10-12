4271bcbe32c8098a59ec4f1bfea49df6bb9fb187
Ernesto-Alves67 <ernestoalves467@gmail.com>
2025-10-12 00:18:18 -0300

    [FIX] detalhe versao

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "v1.10.0"
+        versionName = "v1.10.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
