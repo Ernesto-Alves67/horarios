c0cd663bd02642a531574542cb50029828b9acbc
Ernesto-Alves67 <ernestoalves467@gmail.com>
2025-11-21 19:57:05 -0300

    [Fix] ssl error on SigaaWebView.

--- app/src/main/java/com/scherzolambda/horarios/ui/screens/web/SigaaWebView.kt
+++ app/src/main/java/com/scherzolambda/horarios/ui/screens/web/SigaaWebView.kt
@@ -2,14 +2,18 @@ package com.scherzolambda.horarios.ui.screens.web
 
 import android.annotation.SuppressLint
 import android.net.http.SslError
+import android.util.Log
+import android.view.View
 import android.webkit.SslErrorHandler
-import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 
 @SuppressLint("SetJavaScriptEnabled")
@@ -24,9 +29,10 @@ import androidx.compose.ui.viewinterop.AndroidView
 fun SigaaWebView(
     modifier: Modifier = Modifier,
     url: String = "https://sigaa.sistemas.ufcat.edu.br/sigaa/mobile/touch/public/principal.jsf",
-    webViewRef: (WebView?) -> Unit = {},
+    webViewRef: (WebView) -> Unit = {},
 ) {
     var isLoading by remember { mutableStateOf(true) }
+    var isSSLError by remember { mutableStateOf(false) }
 
     Box(modifier = modifier) {
         AndroidView(
@@ -36,33 +42,58 @@ fun SigaaWebView(
                     settings.apply {
                         javaScriptEnabled = true
                         domStorageEnabled = true
-                        // Habilitar zoom
                         setSupportZoom(true)
                         builtInZoomControls = true
-                        displayZoomControls = true // Oculta botões de zoom para uma UI mais limpa
+                        displayZoomControls = true
                         useWideViewPort = true
-//                        loadWithOverviewMode = true
-                        // Otimizar cache e renderização
-                        cacheMode = android.webkit.WebSettings.LOAD_DEFAULT
-                        allowFileAccess = false // Segurança
-                        allowContentAccess = false // Segurança
+                        loadWithOverviewMode = true
+                        clearCache(true)
                     }
-                    setLayerType(WebView.LAYER_TYPE_HARDWARE, null)
+
                     webViewClient = object : WebViewClient() {
                         @SuppressLint("WebViewClientOnReceivedSslError")
                         override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler?, error: SslError?) {
-                            handler?.proceed()
+//                            handler?.proceed()
+                            val primaryError_ = error?.primaryError
+                            when(primaryError_){
+                                SslError.SSL_UNTRUSTED -> {
+                                    // Certificado não é confiável
+                                    Log.d("SigaaWebView", "SSL_UNTRUSTED error>>>>")
+                                    handler?.cancel()
+                                }
+                                SslError.SSL_EXPIRED -> {
+                                    // Certificado expirado
+                                    handler?.cancel()
+                                }
+                                SslError.SSL_IDMISMATCH -> {
+                                    // Nome do host não corresponde
+                                    handler?.cancel()
+                                }
+                                SslError.SSL_NOTYETVALID -> {
+                                    // Certificado ainda não é válido
+                                    handler?.cancel()
+                                }
+                                SslError.SSL_DATE_INVALID -> {
+                                    // Data inválida no certificado
+                                    handler?.cancel()
+                                }
+                                SslError.SSL_INVALID -> {
+                                    // Erro genérico de SSL
+                                    handler?.cancel()
+                                }
+                                else -> {
+                                    handler?.cancel()
+                                }
+                            }
+                            Log.d("SigaaWebView", "SSL Error encountered: $error")
+                            isSSLError = true
                         }
 
                         override fun onPageFinished(view: WebView?, url: String?) {
                             isLoading = false
                         }
                     }
-                    webChromeClient = object : WebChromeClient() {
-                        override fun onProgressChanged(view: WebView?, newProgress: Int) {
-                            isLoading = newProgress < 100
-                        }
-                    }
+
                     loadUrl(url)
                     webViewRef(this)
                 }
@@ -75,6 +106,23 @@ fun SigaaWebView(
                 modifier = Modifier.align(Alignment.Center)
             )
         }
+
+        if (isSSLError){
+            Column (
+                modifier = Modifier.align(Alignment.Center)
+            ){
+                Image(
+                    painter = androidx.compose.ui.res.painterResource(id = com.scherzolambda.horarios.R.drawable.ic_alert),
+                    contentDescription = "SSL Error",
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = "Erro no certificado SSL.\nA conexão não é segura.\n \n Fale com o suporte.",
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontSize = 23.sp
+                )
+            }
+        }
     }
 }
 
@@ -86,6 +134,7 @@ fun SigaaWebScreen(
     SigaaWebView(
         modifier = Modifier.fillMaxSize(),
         webViewRef = { webView ->
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
             webViewRef(webView)
         },
     )
